07c02f98321d7474863a91867549746f9f1c8e6e
KevinCosma <kcosman2005@gmail.com>
2021-10-07 15:38:14 -0500

    Finished the hero table.

--- src/components/SuperHeroTable/SuperHeroTable.jsx
+++ src/components/SuperHeroTable/SuperHeroTable.jsx
@@ -4,31 +4,13 @@ const SuperHeroTable = (props) => {
     return ( 
         <div>
         {props.heroTable.map(function(superHero){
-            return
+            return (
             <table>
-                <tr>
-                    <th>heroIdOne</th>
-                    <th>heroIdTwo</th>
-                    <th>heroIdThree</th>
-                </tr>
-                <tr>
-                    <td>heroNameOne</td>
-                    <td>heroNameTwo</td>
-                    <td>heroNameThree</td>
-                </tr>
-                <tr>
-                    <td>primaryAbilityOne</td>
-                    <td>primaryAbilityTwo</td>
-                    <td>primaryAbilityThree</td>
-                </tr>
-                <tr>
-                    <td>secondarAbilityOne</td>
-                    <td>secondarAbilityTwo</td>
-                    <td>secondaryAbilityThree</td>
-                </tr> 
+                <tr><th>Hero ID</th><th>Hero Name</th><th>Primary Ability</th><th>Secondary Ability</th></tr>
+                <tr><td>{superHero.superheroId}</td>{superHero.name}<td>{superHero.primaryAbility}</td><td>{superHero.secondarAbility}</td></tr>
             </table>
-        })}
-    </div>
+        )})}
+        </div>
      );
 }
  
